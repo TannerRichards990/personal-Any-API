6b8c50c00bbd79d9dd6ae9c595679d7422eb4a94
Tanner Richards <Tanner.richards990@gmail.com>
2022-12-08 12:42:10 -0800

    built out rocketleague controller and model, wrote test and got it to work

--- __tests__/app.test.js
+++ __tests__/app.test.js
@@ -1,15 +1,32 @@
 const pool = require('../lib/utils/pool');
 const setup = require('../data/setup');
-// const request = require('supertest');
-// const app = require('../lib/app');
+const request = require('supertest');
+const app = require('../lib/app');
+
+const rlGame = require('../lib/models/rlModel');
 
 describe('backend-express-template routes', () => {
   beforeEach(() => {
     return setup(pool);
   });
-  it('example test - delete me!', () => {
-    expect(1).toEqual(1);
+
+  it('/rocket league should render a list of all osu players', async () => {
+    const res = await request(app).get('/rocketleague');
+    const allPlayers = await rlGame.getAllPlayers();
+    const expected = allPlayers.map((rocketleague) => {
+      return {
+        id: rocketleague.id,
+        name: rocketleague.name,
+        platform: rocketleague.platform,
+        country: rocketleague.country,
+      };
+    });
+    expect(res.body).toEqual(expected);
   });
+
+  
+
+
   afterAll(() => {
     pool.end();
   });
